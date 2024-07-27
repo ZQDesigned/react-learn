a601410712621585a55ffbf9b6aa02f251090ece
ZQDesigned <2990918167@QQ.COM>
2024-07-27 15:20:43 +0800

    feat: implement custom hook 'useMount' and 'useDebounce'

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import { useEffect, useState } from "react";
-import { cleanObject } from "../../utils";
+import { cleanObject, useDebounce, useMount } from "../../utils";
 import * as qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -12,25 +12,26 @@ export const ProjectListScreen = () => {
     name: '',
     personId: ''
   })
+  const debouncedParam = useDebounce(param, 2000)
   const [users, setUsers] = useState([])
   const [list, setList] = useState([])
 
   useEffect(() => {
     // eslint-disable-next-line no-undef
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if (response.ok) {
         setList(await response.json())
       }
     })
   }, [param]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, []);
+  });
 
   return <div>
     <SearchPanel param={param} setParam={setParam} users={users}/>
