f3b4a6289a9158076d4524f93a60b3a77a6d5e83
ZQDesigned <2990918167@QQ.COM>
2024-07-27 15:57:21 +0800

    chore: support ts

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,43 @@
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { useEffect, useState } from "react";
+import { cleanObject, useDebounce, useMount } from "../../utils";
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 200);
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`,
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
