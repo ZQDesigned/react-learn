70cf33541207a95384bd301ea2b8c20960f52e6e
ZQDesigned <2990918167@QQ.COM>
2024-08-02 12:57:21 +0800

    feat: support login and register UI logic

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,8 +1,8 @@
 import { FormEvent } from "react";
-import { useAuth } from "../../context/auth-context";
+import { useAuth } from "../context/auth-context";
 
-export const LoginScreen = () => {
-  const { login, user } = useAuth();
+export const RegisterScreen = () => {
+  const { register, user } = useAuth();
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -10,16 +10,10 @@ export const LoginScreen = () => {
       .value;
     const password = (event.currentTarget.elements[1] as HTMLInputElement)
       .value;
-    login({ username, password });
+    register({ username, password });
   };
   return (
     <form onSubmit={handleSubmit}>
-      {user ? (
-        <div>
-          登录成功，用户名：{user?.name}
-          {user?.token}
-        </div>
-      ) : null}
       <div>
         <label htmlFor="username">用户名</label>
         <input type="text" id="username" />
@@ -28,7 +22,7 @@ export const LoginScreen = () => {
         <label htmlFor="password">密码</label>
         <input type="password" id="password" />
       </div>
-      <button type="submit">登录</button>
+      <button type="submit">注册</button>
     </form>
   );
 };
